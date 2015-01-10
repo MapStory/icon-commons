ba40ecb89cd1b88f1f4e6ba448f964ac2ff87b83
Ian Schneider <planablediglet@gmail.com>
2015-01-10 09:55:32 -0700

    make manage.py fail on non test commands
    after attempting to use quite unsuccessfully while sleepy

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    if 'test' not in sys.argv:
+        print 'this is here only for testing'
+        sys.exit(1)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.test_settings")
 
     from django.core.management import execute_from_command_line
